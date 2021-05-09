591314eb549ba38ce6c1546b5f667185bc5c11d3
sonerkucuk <sonerkucuk5561@gmail.com>
2021-05-09 19:44:50 +0200

    I have update  my bla bla

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("HelloTest");
+        System.out.println("HelloTest Merhaba");
+        //more changes  are comings
+        //I am ready to commit
     }
 }
